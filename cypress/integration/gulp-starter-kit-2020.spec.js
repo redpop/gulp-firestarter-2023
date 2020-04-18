27b6a2c1ba726a106c58618235d16af838a480d2
Martin Alker <martin.alker@mailbox.org>
2020-04-18 22:44:27 +0200

    Add cypress

---
+++ cypress/integration/gulp-starter-kit-2020.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable func-names */
+
+describe('The index page', function () {
+    it('successfully loads', function () {
+        cy.visit('/'); // change URL to match your dev URL
+    });
+});
+
+/* eslint-enable func-names */

4519322788fc290f710e061987fa6153e44daa67
Martin Alker <martin.alker@mailbox.org>
2020-04-20 22:06:51 +0200

    Add gulp-changed (for images task)

--- gulpfile.esm.js/images.js
+++ gulpfile.esm.js/images.js
@@ -1,4 +1,5 @@
 import gulp from 'gulp';
+import changed from 'gulp-changed';
 import plugins from 'gulp-load-plugins';
 
 import * as config from './config';
@@ -8,6 +9,7 @@ const $ = plugins();
 export default function images() {
     return gulp
         .src(`${config.PATHS.src.images}/**/*`)
+        .pipe(changed(`${config.PATHS.dist.images}`))
         .pipe($.if(config.PRODUCTION, $.imagemin()))
         .pipe(gulp.dest(config.PATHS.dist.images));
 }

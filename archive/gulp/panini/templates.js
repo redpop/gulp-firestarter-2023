64c2a1a83af548adefb2feb1afa7e0ee1fbaa365
Martin Alker <martin.alker@mailbox.org>
2020-05-04 19:08:57 +0200

    Add archive folder with old/deprecated gulp tasks

---
+++ archive/gulp/panini/templates.js
@@ -0,0 +1,24 @@
+import gulp from 'gulp';
+import panini from 'panini';
+
+import * as config from './config';
+
+export default function templates() {
+    return gulp
+        .src(`${config.PATHS.src.templates.pages}/**/*.html`)
+        .pipe(
+            panini({
+                root: `${config.PATHS.src.templates.pages}`,
+                layouts: `${config.PATHS.src.templates.layouts}`,
+                partials: `${config.PATHS.src.templates.partials}`,
+                data: `${config.PATHS.src.templates.data}`,
+                helpers: `${config.PATHS.src.templates.helpers}`,
+            })
+        )
+        .pipe(gulp.dest(config.PATHS.dist.folder));
+}
+
+export function resetTemplates(done) {
+    panini.refresh();
+    done();
+}

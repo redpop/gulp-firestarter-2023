b307c8a3fcdab8d78b24dc9182286a7a91e9d1a5
Martin Alker <martin.alker@mailbox.org>
2020-06-13 22:07:31 +0200

    Fix cypress test

--- cypress/integration/gulp-firestarter-2020.spec.js
+++ cypress/integration/gulp-firestarter-2020.spec.js
@@ -3,7 +3,7 @@
 describe('The index page', function () {
     it('successfully loads and has hero title', function () {
         cy.visit('/');
-        cy.contains('Gulp starter kit 2020');
+        cy.contains('Gulp firestarter 2020');
     });
 });
 
